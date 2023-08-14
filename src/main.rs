de53341dfd1ee79c75042f7f461843cab46fb599
gitdog01 <hostdog01@naver.com>
2023-08-14 17:40:04 +0900

    Make data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::collections::HashMap;
+
+struct Minimemcached {
+    items: HashMap<String, i32>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut mm = Minimemcached {
+        items: HashMap::new(),
+    };
+
+    mm.items.insert("first_key".to_string(), 42);
+    mm.items.insert("second_key".to_string(), 123);
+
+    println!("hello");
 }
